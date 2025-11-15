92d6cbedfff296a1a5037bec4f7a7c885e7f5370
tunnu-sahni <tsahni313@gmail.com>
2025-11-15 17:03:57 +0530

    added datacsv.py

---
+++ datacsv.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+print(df.to_string()) 
+
+
+
+import pandas as pd 
+
+df = pd.read_csv('data.csv')
+
+print(df)
+
+
+
+# max_rows
+
+
+import pandas as pd
+
+print(pd.options.display.max_rows)
+
+
+
+import pandas as pd
+
+pd.options.display.max_rows = 9999
+
+df = pd.read_csv('data.csv')
+
+print(df)
